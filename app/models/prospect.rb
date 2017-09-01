59abe028ff6dbe061faa9e0051f5d69e116c6eab
Matthias Vitte <vittematthias@gmail.com>
2017-09-01 12:36:21 +0200

    adding sendgrid

--- app/models/prospect.rb
+++ app/models/prospect.rb
@@ -2,4 +2,13 @@ class Prospect < ApplicationRecord
   validates :email, presence: true,
   uniqueness: true,
   format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i, message: "Ceci n'est pas un email" }
+
+  after_create :send_welcome_email
+
+  private
+
+  def send_welcome_email
+    ProspectMailer.welcome(self).deliver_now
+  end
+  
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,7 +57,17 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "testing-facebookads_#{Rails.env}"
   config.action_mailer.perform_caching = false
-
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { host: "http://testing-facebookads.herokuapp.com" }
+  config.action_mailer.smtp_settings = {
+    :address   => "smtp.sendgrid.net",
+    :port      => 587, # ports 587 and 2525 are also supported with STARTTLS
+    :enable_starttls_auto => true, # detects and uses STARTTLS
+    :user_name => ENV["SENDGRID_USERNAME"],
+    :password  => ENV["SENDGRID_PASSWORD"], # SMTP password is any valid API key, when user_name is "apikey".
+    :authentication => 'login',
+    :domain => 'testing-facebookads.herokuapp.com', # your domain to identify your server when connecting
+}
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
@@ -86,4 +96,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+
+
+
 end
